80de7f774df41591d6993169db79ec9d4032f5a8
Daniel Platz <daniel.platz@me.com>
2018-04-07 22:58:58 +0200

    configure auto-deployment

---
+++ websphere-9/configure.py
@@ -0,0 +1,13 @@
+import java.lang.System as sys
+
+cell = AdminConfig.getid('/Cell:DefaultCell01/')
+md = AdminConfig.showAttribute(cell, "monitoredDirectoryDeployment")
+AdminConfig.modify(md, [['enabled', "true"]])
+AdminConfig.modify(md, [['pollingInterval', "1"]])
+#AdminConfig.modify(md, [['monitoredDirectory', "/newPath"]])
+
+print AdminConfig.show(md)
+
+AdminConfig.save()
+
+print 'Done.'
---
+++ websphere-9/configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+PROFILE_NAME=${PROFILE_NAME:-"AppSrv01"}
+SERVER_NAME=${SERVER_NAME:-"server1"}
+
+start_server()
+{
+    echo "Starting server ..................."
+    /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startServer.sh $SERVER_NAME
+}
+
+stop_server()
+{
+    echo "Stopping server ..................."
+    /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/stopServer.sh $SERVER_NAME
+}
+
+start_server
+/opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -lang jython -f /work/configure.py
+stop_server
