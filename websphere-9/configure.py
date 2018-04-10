af9caef0b15f8925f017e3e3dab20d950ac79921
Daniel Platz <daniel.platz@me.com>
2018-04-10 07:20:49 +0200

    Remove DefaultApplication as it occupies the default context-root

--- websphere-9/configure.py
+++ websphere-9/configure.py
@@ -1,5 +1,7 @@
 import java.lang.System as sys
 
+AdminApp.uninstall('DefaultApplication')
+
 cell = AdminConfig.getid('/Cell:DefaultCell01/')
 md = AdminConfig.showAttribute(cell, "monitoredDirectoryDeployment")
 AdminConfig.modify(md, [['enabled', "true"]])
