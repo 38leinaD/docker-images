22421ffd4701aa4e52056a2d41b7077fa7dce415
Daniel Platz <daniel.platz@me.com>
2018-04-11 08:07:34 +0200

    Enable develop- and debug-mode

--- websphere-9/configure.py
+++ websphere-9/configure.py
@@ -1,12 +1,24 @@
 import java.lang.System as sys
 
-AdminApp.uninstall('DefaultApplication')
-
+server = AdminConfig.getid('/Server:server1/')
 cell = AdminConfig.getid('/Cell:DefaultCell01/')
+
+# Uninstall all default apps
+AdminApp.uninstall('DefaultApplication')
+AdminApp.uninstall('ivtApp')
+AdminApp.uninstall('query')
+
+# configure auto-deploy folder
 md = AdminConfig.showAttribute(cell, "monitoredDirectoryDeployment")
 AdminConfig.modify(md, [['enabled', "true"]])
 AdminConfig.modify(md, [['pollingInterval', "1"]])
+# Defalt is "/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/monitoredDeployableApps"
 #AdminConfig.modify(md, [['monitoredDirectory', "/newPath"]])
+
+# Enable debug port
+AdminTask.setJVMProperties('[-nodeName DefaultNode01 -serverName server1 -debugMode true -debugArgs "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=7777"]')
+# Enable development-mode
+AdminConfig.modify(server, [['developmentMode', 'true']])
 
 print AdminConfig.show(md)
 
