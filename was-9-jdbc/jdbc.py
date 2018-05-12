77326f17894bbd88251620a61f07c0f7ed6b666e
Daniel Platz <daniel.platz@me.com>
2018-05-12 22:56:52 +0200

    renamed datasources

--- was-9-jdbc/jdbc.py
+++ was-9-jdbc/jdbc.py
@@ -12,7 +12,7 @@ def findJAASAlias(pAlias):
 AdminTask.createAuthDataEntry('[-alias %s -user %s -password %s -description \'%s\']'%('oracle', 'system', 'oracle', 'oracle user'))
 oracleCredentials = findJAASAlias('oracle')
 oracleXA = AdminTask.createJDBCProvider('[-scope Server=server1 -databaseType Oracle -providerType "Oracle JDBC Driver" -implementationType "XA data source" -name "Oracle JDBC Driver (XA)" -description "Oracle JDBC Driver" -classpath [ "/opt/ojdbc7.jar" ] -nativePath "" ]')
-AdminTask.createDatasource(oracleXA, '[-name %s -jndiName %s -dataStoreHelperClassName com.ibm.websphere.rsadapter.Oracle11gDataStoreHelper -containerManagedPersistence %s -componentManagedAuthenticationAlias %s -configureResourceProperties [[URL java.lang.String %s]]]'%('ORACLE_DS', 'ORACLE_DS', 'false', oracleCredentials, 'jdbc:oracle:thin:@oracle:1521/XE'))
+AdminTask.createDatasource(oracleXA, '[-name %s -jndiName %s -dataStoreHelperClassName com.ibm.websphere.rsadapter.Oracle11gDataStoreHelper -containerManagedPersistence %s -componentManagedAuthenticationAlias %s -configureResourceProperties [[URL java.lang.String %s]]]'%('ORACLE', 'ORACLE', 'false', oracleCredentials, 'jdbc:oracle:thin:@oracle:1521/XE'))
 
 AdminConfig.save()
 
