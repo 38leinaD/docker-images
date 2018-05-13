3c9714eb461ca4b2ba7735b564d6055664856395
Daniel Platz <daniel.platz@me.com>
2018-05-13 14:24:13 +0200

    WLP image supporting Derby, Postgres and Oracle

---
+++ wlp-javaee7-jdbc/README.md
@@ -0,0 +1,8 @@
+# Wildfly 10 Docker image
+
+This is a docker-image is based on the [offical Websphere Liberty images](https://hub.docker.com/_/websphere-liberty/).
+Additionally, it contains modules for integration with popular databases:
+
+* PostgreSQL 9.5
+* Oracle 11/12
+* Apache Derby
---
+++ wlp-javaee7-jdbc/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ ! -f postgresql-9.4.1209.jar ]; then
+    curl -O https://jdbc.postgresql.org/download/postgresql-9.4.1209.jar
+fi
+
+if [ ! -f ojdbc7.jar ]; then
+    echo "Download ojdbc7.jar from http://www.oracle.com/technetwork/database/features/jdbc/jdbc-drivers-12c-download-1958347.html"
+    exit 1
+fi
+
+if [ ! -f db-derby-10.14.2.0-lib.zip ]; then
+    curl -O http://apache.mirror.iphh.net//db/derby/db-derby-10.14.2.0/db-derby-10.14.2.0-lib.zip
+fi
+
+if [ ! -d db-derby-10.14.2.0-lib ]; then
+    unzip db-derby-10.14.2.0-lib.zip
+fi
+
+docker build -t 38leinad/wlp-javaee7-jdbc:latest .
