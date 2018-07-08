27d050b85faa4fd72215b894f77b036507d87dc4
Daniel Platz <daniel.platz@me.com>
2018-07-08 16:31:29 +0200

    Add was:9.0.0.7

--- was-9/build.sh
+++ was-9/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 docker build -t 38leinad/was:latest .
-docker tag 38leinad/was-9:latest 38leinad/was:9.0.0.6
+docker tag 38leinad/was-9:latest 38leinad/was:9.0.0.7
 
 if [[ "$1" == "push" ]]; then
     docker push 38leinad/was:latest
-    docker push 38leinad/was:9.0.0.6
+    docker push 38leinad/was:9.0.0.7
 fi
