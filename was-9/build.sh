6fa1c962783a984f935efdc390742475f430cda2
Daniel Platz <daniel.platz@me.com>
2018-07-08 15:24:09 +0200

    Better versioning for was container

--- was-9/build.sh
+++ was-9/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
-docker build -t 38leinad/was-9:latest .
+docker build -t 38leinad/was:latest .
+docker tag 38leinad/was-9:latest 38leinad/was:9.0.0.6
+
+if [[ "$1" == "push" ]]; then
+    docker push 38leinad/was:latest
+    docker push 38leinad/was:9.0.0.6
+fi
